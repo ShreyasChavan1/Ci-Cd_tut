ad257d9f6cf428da684bcad716cf06a4b0cf2ca0
ShreyasChavan1 <152523720+ShreyasChavan1@users.noreply.github.com>
2025-12-19 20:01:53 +0530

    fixed change

--- index.js
+++ index.js
@@ -1,18 +1,21 @@
 const Redis = require('ioredis')
 
 const redisclient = new Redis({
-    host: process.env.host,
-    port: process.env.port
+    host: process.env.HOST,
+    port: process.env.PORT
 })
 
 async function checkredis(){
+    
     try{
         await redisclient.set("user","works")
         const value = await redisclient.get("user");
-    
         console.log(value)
     }catch{
         console.error("something went wrong here")
+    }finally{
+        await redisclient.disconnect()
     }
 }
+
 checkredis()
