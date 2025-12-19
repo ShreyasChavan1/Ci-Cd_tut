da6a8d261d2d0d1c142a3158f6f8fa97b3facaa1
ShreyasChavan1 <152523720+ShreyasChavan1@users.noreply.github.com>
2025-12-19 19:40:47 +0530

    added redis service

--- index.js
+++ index.js
@@ -1,2 +1,18 @@
-console.log("ENV: ",process.env.APP_ENV)
-console.log("PORT: ",process.env.PORT)
+import Redis from "ioredis";
+
+const redisclient = new Redis({
+    host: process.env.host,
+    port: process.env.port
+})
+
+async function checkredis(){
+    try{
+        await redisclient.set("user","works")
+        const value = await redisclient.get("user");
+    
+        console.log(value)
+    }catch{
+        console.error("something went wrong here")
+    }
+}
+checkredis()
